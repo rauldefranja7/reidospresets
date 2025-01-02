a0b12ba7db5d88d9089f8b552531243a2d6ef63e
rauldefranja7 <sindicoprofissional360@gmail.com>
2025-01-02 16:36:44 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,135 @@
+// JavaScript Document
+;(function($) {
+		
+	"use strict";
+	if( $( '.perch-vc-carousel' ).length > 0 ){
+		$( '.perch-vc-carousel' ).each(function(){
+			var carousel = $(this);
+			var type = carousel.data( 'carousel_type' );
+			var column_lg = parseInt(carousel.data( 'column_lg' ));
+			var column_md = parseInt(carousel.data( 'column_md' ));
+			var column_sm = parseInt(carousel.data( 'column_sm' ));
+			var column_xs = parseInt(carousel.data( 'column_xs' ));
+			var autoplay = Boolean(carousel.data( 'autoplay' ));
+			var center = Boolean(carousel.data( 'center' ));
+			var dots = Boolean(carousel.data( 'dots' ));
+			var navs = Boolean(carousel.data( 'navs' ));
+			var loop = Boolean(carousel.data( 'loop' ));
+			var margin = parseInt(carousel.data( 'margin' ));
+
+
+			if( carousel.hasClass( 'perch-vc-carousel-owl' )  && ( type == 'owl' ) ){
+				var owl_options = {
+						items: column_lg,
+						slideBy: column_lg,
+						dotsEach: column_lg,
+						loop: loop,
+						autoplay:autoplay,
+						dots: dots,
+						nav : navs,
+						dotsEach: true,
+						navText: ["<",">"],
+						margin: margin,
+						center: center,
+						animateOut: 'fadeOut',
+						animateIn: 'fadeIn',
+						autoplayTimeout: 4500,
+						autoplayHoverPause: true,
+						smartSpeed: 1500,
+						responsive:{
+							0:{
+								items: column_xs,
+								slideBy: column_xs,
+								dotsEach: column_xs,
+							},
+							600:{
+								items: column_xs,
+								slideBy: column_xs,
+								dotsEach: column_xs,
+							},
+							768:{
+								items: column_sm,
+								slideBy: column_sm,
+								dotsEach: column_sm,
+							},
+							991:{
+								items: column_md,
+								slideBy: column_md,
+								dotsEach: column_md,
+							},
+							1000:{
+								items: column_lg,
+								slideBy: column_lg,
+								dotsEach: column_lg,
+							}
+						}
+				};
+				carousel.owlCarousel(owl_options);
+			}
+
+			if( carousel.hasClass( 'perch-vc-carousel-slick' ) && ( type == 'slick' ) ){
+				var slick_options = {
+					centerMode: center,
+					autoplay: autoplay,
+					centerPadding: '60px',
+					speed: 800,			
+					slidesToShow: column_lg,
+					dots: dots,
+					arrows: navs,
+					responsive: [
+						{
+							breakpoint: 1199,
+							settings: {
+							arrows: navs,
+							centerMode: center,
+							centerPadding: margin+'px',
+							slidesToShow: column_lg
+							}
+						},
+						{
+							breakpoint: 991,
+							settings: {
+							arrows: navs,
+							centerMode: center,
+							centerPadding: margin+'px',
+							slidesToShow: column_lg
+							}
+						},
+						{
+							breakpoint: 800,
+							settings: {
+							arrows: navs,
+							centerMode: center,
+							centerPadding: margin+'px',
+							slidesToShow: column_md
+							}
+						},
+						{
+							breakpoint: 767,
+							settings: {
+							arrows: navs,
+							centerMode: center,
+							centerPadding: margin+'px',
+							slidesToShow: column_sm
+							}
+						},
+						{
+							breakpoint: 648,
+							settings: {
+							arrows: navs,
+							centerMode: center,
+							centerPadding: margin+'px',
+							slidesToShow: column_xs
+							}
+						}
+					]
+				};
+
+				
+				carousel.slick(slick_options);
+			}
+
+		});
+	}
+
+})( jQuery, window, document );
